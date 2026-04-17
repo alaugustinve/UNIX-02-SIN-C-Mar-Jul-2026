c756e1964e69063ab7c69d5e99271aaa589fb73f
alaugustinve <alaugustinve@uide.edu.ec>
2026-04-17 14:59:34 +0000

    commit just incase

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -1,7 +1,7 @@
 @alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -a
 .  ..  .git  .gitignore  LICENSE  README.md  boot_exploration.sh
 @alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls --all
-.  ..  .git  .gitignore  LICENSE  README.md  boot_exploration.sh *this commandsshows every file, hidden or not** 
+.  ..  .git  .gitignore  LICENSE  README.md  boot_exploration.sh #this commandsshows every file, hidden or not** 
 @alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -l -ah
 total 64K
 drwxrwxrwx+ 3 codespace root      4.0K Apr 17 14:24 .
@@ -19,15 +19,15 @@ drwxrwxrwx+ 8 codespace root      4.0K Apr 17 14:23 .git
 -rw-rw-rw-  1 codespace root      4.6K Apr 17 14:22 .gitignore
 -rw-rw-rw-  1 codespace root       34K Apr 17 14:22 LICENSE
 -rw-rw-rw-  1 codespace root        53 Apr 17 14:22 README.md
--rw-rw-rw-  1 codespace codespace  350 Apr 17 14:26 boot_exploration.sh **These 3 commands function as the same, they are useful to make the commands lines legible for humans**
+-rw-rw-rw-  1 codespace codespace  350 Apr 17 14:26 boot_exploration.sh #These 3 commands function as the same, they are useful to make the commands lines legible for humans**
 @alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ rmdir -- -rf 
 rmdir: failed to remove '-rf': No such file or directory
-@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ mkdir -- -rf **Creates the folder with the uncommon name**
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ mkdir -- -rf  #Creates the folder with the uncommon name**
 @alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ rdmir -- -rf
-bash: rdmir: command not found                                                             **Typing error**
-@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ rmdir -- -rf **removes the folder**
+bash: rdmir: command not found                                                            #Typing error**
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ rmdir -- -rf  #removes the folder**
 
-@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls --help **This command shows a list of commands for the user to use as a guide**
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls --help  #This command shows a list of commands for the user to use as a guide**
 Usage: ls [OPTION]... [FILE]...
 List information about the FILEs (the current directory by default).
 Sort entries alphabetically if none of -cftuvSUX nor --sort is specified.
@@ -167,11 +167,33 @@ GNU coreutils online help: <https://www.gnu.org/software/coreutils/>
 Report any translation bugs to <https://translationproject.org/team/>
 Full documentation <https://www.gnu.org/software/coreutils/ls>
 or available locally via: info '(coreutils) ls invocation'
-@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ man ls **This command shows a manual. The controls for this manual are, lowercase "n" to go forward uppercase "n". You can use / to look for a word. You quit the manual by pressing q**
-@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ man git-clone **This command shows the manual of git-clone, you can look for explanations and commands here aswell**
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ man ls #This command shows a manual. The controls for this manual are, lowercase "n" to go forward uppercase "n". You can use / to look for a word. You quit the manual by pressing q**
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ man git-clone #This command shows the manual of git-clone, you can look for explanations and commands here aswell**
 --depth <depth>
            Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If
            you want to clone submodules shallowly, also pass --shallow-submodules.
            @alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -l
 total 52
--rw-rw-rw- 1 codespace codespace 10919 Apr 17 14:45 boot_exploration.sh **The first variable defines the type of file it is, the first 3 variables group shows which permissions the user has, the second 3 variables shows the permissions for the group and the last 3 shows the permissions for others**
+-rw-rw-rw- 1 codespace codespace 10919 Apr 17 14:45 boot_exploration.sh #The first variable defines the type of file it is, the first 3 variables group shows which permissions the user has, the second 3 variables shows the permissions for the group and the last 3 shows the permissions for others**
+CHMOD QUIÉN OPERA CON QUÉ PERMISOS SOBRE QUÉ ARCHIVO)
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ touch script.sh #Creates a new file named script.sh
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -l #Permission verification
+total 52
+-rw-rw-rw- 1 codespace root      34523 Apr 17 14:22 LICENSE
+-rw-rw-rw- 1 codespace root         53 Apr 17 14:22 README.md
+-rw-rw-rw- 1 codespace codespace 11362 Apr 17 14:54 boot_exploration.sh
+-rw-rw-rw- 1 codespace codespace     0 Apr 17 14:55 script.sh
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ chmod +x script.sh #Gives permission to everyone
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -l #Permission verification
+total 52
+-rw-rw-rw- 1 codespace root      34523 Apr 17 14:22 LICENSE
+-rw-rw-rw- 1 codespace root         53 Apr 17 14:22 README.md
+-rw-rw-rw- 1 codespace codespace 11884 Apr 17 14:56 boot_exploration.sh
+-rwxrwxrwx 1 codespace codespace     0 Apr 17 14:55 script.sh
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ chmod u+x script.sh
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -l
+total 52
+-rw-rw-rw- 1 codespace root      34523 Apr 17 14:22 LICENSE
+-rw-rw-rw- 1 codespace root         53 Apr 17 14:22 README.md
+-rw-rw-rw- 1 codespace codespace 12288 Apr 17 14:57 boot_exploration.sh
+-rwxrwxrwx 1 codespace codespace     0 Apr 17 14:55 script.sh
