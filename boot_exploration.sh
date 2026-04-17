6a2864f2ec2a06e48355c5b2da0e41729ed27d3e
alaugustinve <alaugustinve@uide.edu.ec>
2026-04-17 14:27:58 +0000

    explanation first commands

---
+++ boot_exploration.sh
@@ -0,0 +1,22 @@
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -a
+.  ..  .git  .gitignore  LICENSE  README.md  boot_exploration.sh
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls --all
+.  ..  .git  .gitignore  LICENSE  README.md  boot_exploration.sh *this commandsshows every file, hidden or not** 
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -l -ah
+total 64K
+drwxrwxrwx+ 3 codespace root      4.0K Apr 17 14:24 .
+drwxr-xrwx+ 5 codespace root      4.0K Apr 17 14:22 ..
+drwxrwxrwx+ 8 codespace root      4.0K Apr 17 14:23 .git
+-rw-rw-rw-  1 codespace root      4.6K Apr 17 14:22 .gitignore
+-rw-rw-rw-  1 codespace root       34K Apr 17 14:22 LICENSE
+-rw-rw-rw-  1 codespace root        53 Apr 17 14:22 README.md
+-rw-rw-rw-  1 codespace codespace  350 Apr 17 14:26 boot_exploration.sh
+@alaugustinve ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -lah
+total 64K
+drwxrwxrwx+ 3 codespace root      4.0K Apr 17 14:24 .
+drwxr-xrwx+ 5 codespace root      4.0K Apr 17 14:22 ..
+drwxrwxrwx+ 8 codespace root      4.0K Apr 17 14:23 .git
+-rw-rw-rw-  1 codespace root      4.6K Apr 17 14:22 .gitignore
+-rw-rw-rw-  1 codespace root       34K Apr 17 14:22 LICENSE
+-rw-rw-rw-  1 codespace root        53 Apr 17 14:22 README.md
+-rw-rw-rw-  1 codespace codespace  350 Apr 17 14:26 boot_exploration.sh **These 3 commands function as the same, only
