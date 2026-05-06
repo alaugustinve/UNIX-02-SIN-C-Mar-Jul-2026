11766948711f3f5a796daa366949310d0fdeed5d
alaugustinve <alaugustinve@uide.edu.ec>
2026-05-06 13:01:45 +0000

    Commit 1: Linux_github history creation

---
+++ Linux/script_linux_cisco_1.sh
@@ -0,0 +1,16 @@
+                                      
+    1  ls                                                                       
+    2  aptitude moo                                                             
+    3  ls -l                                                                    
+    4  -r                                                                       
+    5  ls -r                                                                    
+    6  ls -rl                                                                   
+    7  ls /lr                                                                   
+    8  ls -kr                                                                   
+    9  ls -lr                                                                   
+   10  aptitude -v moo                                                          
+   11  aptitude -vvv moo                                                        
+   12  aptitude -v -v mooo                                                      
+   13  aptitude -v -v                                                           
+   14  history                                                                  
+     
