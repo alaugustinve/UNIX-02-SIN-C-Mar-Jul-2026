8e608f7ac736a34797e67416ad5ca2064a27b0ba
alaugustinve <alaugustinve@uide.edu.ec>
2026-06-01 15:21:34 +0000

    first script finale

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -4,3 +4,17 @@ echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -ef 
+ps -e -f
+df --human-readable 
+df -h
+#!/bin/bash
+set -x
+#!/usr/bin/env bash
+#chmod u+x blackhatbash1.sh
+#bash -x blackhatbash1.sh
+#bash -r blackhatbash1.sh
+#bash -n blackhatbash1.sh
+#./blackhatbash1.sh
+#bash blackhatbash1.sh
+set +x 
