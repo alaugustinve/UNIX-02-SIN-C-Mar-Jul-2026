3ae08752101038c947e35ed5a258e138462c3808
alaugustinve <alaugustinve@uide.edu.ec>
2026-06-12 14:33:30 +0000

    Checking Subsequent Conditions

--- if_elif.sh
+++ if_elif.sh
@@ -1,12 +1,13 @@
-if [[ -z "${USER_INPUT}" ]]; then
+#!/bin/bashif 
+[[ -z "${USER_INPUT}" ]]; then # -z funciona para comprobar si la variable está vacía 
  echo "You must provide an argument!"
- exit 1
+ exit 1 # Termina la ejecución del script con un código de error
 fi
-if [[ -f "${USER_INPUT}" ]]; then
+
+if [[ -f "${USER_INPUT}" ]]; then # -f funciona para comprobar si la ruta es un archivo regular 
  echo "${USER_INPUT} is a file."
-elif [[ -d "${USER_INPUT}" ]]; then
+elif [[ -d "${USER_INPUT}" ]]; then # -d funciona para comprobar si la ruta es un directorio 
  echo "${USER_INPUT} is a directory."
 else
-echo "${USER_INPUT} is not a file or a directory."
+ echo "${USER_INPUT} is not a file or a directory." # Imprime este texto si no es ni archivo ni directorio 
 fi
- # NOMBRE ARCHIOV #NOMBRE CARPETA #ECHO "HOLA"
